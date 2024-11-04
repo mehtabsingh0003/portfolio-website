a64cf3aaca022fc4994cbc48edf2bcc8938ba6ed
mehtab <mehtabsinghr274@gmail.com>
2024-11-04 19:28:26 +0530

    first commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,13 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+import editor from "../../Assets/Projects/readme-img.png";
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
-
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -34,21 +29,11 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="music streaming"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"             
+              title="portfolio-website"
+              description="My portfolio showcases my web development skills through projects using React, Node.js, and responsive design. Each project highlights my ability to integrate APIs and create user-centric applications. It includes links to my GitHub, live demos, and my resume, reflecting my readiness for impactful development roles."
+              ghLink="https://github.com/mehtabsingh0003/portfolio-website.git"             
             />
           </Col>
 
